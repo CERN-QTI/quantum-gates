00bc39c7c31ef6498cda40b5d0fabf5b84044e43
Cherie000 <cherilyn.christen@epfl.ch>
2026-04-07 17:31:28 +0800

    preparation for cloud run

---
+++ main.py
@@ -0,0 +1,5 @@
+from quantum_gates._utility.RotatedSurfaceCodeLoom import RotatedSurfaceCodeLoom
+
+code = RotatedSurfaceCodeLoom(distance=5, n_cycles=5, n_shots=10000, noise=True, p=0.06)
+logical_error_rate = code.run_circ("MrAnderson")
+print(f"Logical error rate: {logical_error_rate:.4f}")
