34f337c750213ca59b41d04c6da9e7e2f59f2f91
Cherie000 <cherilyn.christen@epfl.ch>
2026-04-08 17:11:10 +0800

    preparation for cloud run d=5

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from quantum_gates._utility.RotatedSurfaceCodeLoom import RotatedSurfaceCodeLoom
+import sys
+sys.path.insert(0, "src")
 
 code = RotatedSurfaceCodeLoom(distance=5, n_cycles=5, n_shots=10000, noise=True, p=0.06)
 logical_error_rate = code.run_circ("MrAnderson")
